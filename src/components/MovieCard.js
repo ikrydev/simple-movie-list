64e258f83f619c864d580e211f41f9b03e3cac7d
Akanksha Jai <akankshajai20@gmail.com>
2019-10-23 15:41:51 +0530

    Responsive Image Issue

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const MovieCard = props => (
-        <div className="card">
-            <div className="card-image">
-                <img src={props.movie.image} alt="img"></img>
-            </div>
-            <div className="card-header">
-                <h1 className="card-header-title">{props.movie.title}</h1>
-            </div>
-
-        </div>
-)
+  <div className="card">
+    <div className="card-image">
+      <img style={{ width: "100%" }} src={props.movie.image} alt="img"></img>
+    </div>
+    <div className="card-header">
+      <h1 className="card-header-title">{props.movie.title}</h1>
+    </div>
+  </div>
+);
 export default MovieCard;
